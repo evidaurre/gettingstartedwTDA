c0052abe877bae2905fb3923819beae9fea8e2d9
evidaurre <elizabeth.vidaurre@gmail.com>
2019-05-30 15:53:54 -0400

    Add files via upload

---
+++ manyiudata.py
@@ -0,0 +1,41 @@
+import pandas as pd
+df=pd.read_csv("tda18may.csv")
+
+def genderAssignment(x):
+
+    if x == "M":
+        return 0
+    if x == "F":
+        return 1
+    else:
+        return 0.5
+
+df.Gender = df.Gender.map(genderAssignment)
+
+data = df[["Amount", "Industry Code", "DMA Code", "Gender"]]
+#data['Amount']=data['Amount'].astype(str)
+for i in range(len(data.columns)):
+    x=data.columns[i]
+    data[x]=data[x].astype(str)
+    data[x] = data[x].str.replace(',','')
+    data[x]=data[x].astype(float)
+
+print(type(data.Amount))
+
+data = data.values
+
+
+#now try to mapper
+import kmapper as km
+
+mapper = km.KeplerMapper(verbose=1)
+
+# Fit to and transform the data
+projected_data = mapper.fit_transform(data, projection=[0,1]) # X-Y axis
+
+# Create dictionary called 'graph' with nodes, edges and meta-information
+graph = mapper.map(projected_data, data, nr_cubes=10)
+
+# Visualize it
+mapper.visualize(graph, path_html="manyiudata_may18.html",
+                 title="make_circles(n_samples=5000, noise=0.03, factor=0.3)")
